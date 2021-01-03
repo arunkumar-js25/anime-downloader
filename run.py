dd68164443975277d3bfb7b26f2063713010465f
Arun Kumar <53009824+arun25-coder@users.noreply.github.com>
2021-01-03 21:37:36 +0530

    Update run.py
    Better UI

--- run.py
+++ run.py
@@ -1,26 +1,43 @@
-import requests
-from bs4 import BeautifulSoup
 from gogoanimelink import *
 from Downloader import *
 
-#MAIN
 def main():
-    searchInput = input("Enter Anime Name/URL : ")
-    anime_search_results = GogoAnimeScraper.searchInGogoAnime(searchInput)
+    searchInput = input("\n>> Enter Anime Name/URL : ")
+    anime_search_results = AnimeScraper.searchAnime(searchInput)
     [print(f'\t {i+1}) {anime_search_results[i][0]}') for i in range(len(anime_search_results))]
 
-    selected_index = int(input('\nSelect your option: ')) - 1
-    anime_scraper = GogoAnimeScraper(anime_search_results[selected_index][1])
-    print("\nFOUND:", anime_scraper.episode_count, " Episodes in TOTAL!")
-
-    start_ep = int(input("\nStart From Episode: "))
-    end_ep = int(input("End At Episode: "))
+    selected_index = int(input('\n>> Select your option: ')) - 1
+    anime_scraper = AnimeScraper(anime_search_results[selected_index][1])
+    print("FOUND:", anime_scraper.episode_count, " Episodes in TOTAL!")
+    print("="*40)
+    start_ep = int(input(">> Start From Episode: "))
+    end_ep = int(input(">> End At Episode: "))
+    print("=" * 40)
     anime_scraper.scrapeEpisodes(start=start_ep, end=end_ep)
+    print("=" * 40)
 
-    print("\n-------------------- DOWNLOAD STARTED !!! --------------------")
+    print("DOWNLOAD STARTED !!! ------------------>")
     downloader = Downloader(anime_scraper.dataDict)
-    downloader.downloadAnime()
-    print("\n-------------------- DOWNLOAD COMPLETED !!! ------------------")
+    #downloader.downloadAnime()
+    print("DOWNLOAD COMPLETED !!! ---------------->")
+    print("=" * 40)
 
 if __name__ == '__main__':
+    isconfig_done = "DONE"
+    #islog_done = "DONE"
+
+    try:
+        configsetting('config.ini')
+    except:
+        isconfig_done = "FAILED"
+
+    print("""\t"""+("="*33)
+          +"""\n\t|\t|----------------------|\t|\n\t|\t|   ANIME Downloader   |\t|\n\t|\t|----------------------|\t|\n\t|\t Creator : """
+          +str(config['about']['creator']).ljust(11,' ')+"""\t\t|\n\t|\t Version : """
+          +str(config['about']['version']).ljust(11,' ')+"""\t\t|\t\n\t"""
+          +("=" * 33)
+          + """\n\t|\t[Configuration] => """+isconfig_done.ljust(9,' ')
+          #+"""|\n\t|\t[Log Setup]     => """+islog_done.ljust(9,' ')
+          +"""|\n\t"""+("=" * 33))
+
     main()
